47fdf01d8a1ae0787be9ca6bb8d6c4596649f294
Shane Donnelly <112229422+bubbarooski@users.noreply.github.com>
2023-07-19 23:21:54 -0400

    Just testing!

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+using namespace std;
+
+int addition (int arg1, int arg2){
+    int addition = arg1 + arg2;
+    return addition;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int userInput1;
+    int userInput2;
+    int result;
+
+    cout << "Enter number 1: ";
+    cin >> userInput1;
+    cout << "Enter number 2: ";
+    cin >> userInput2;
+
+    result = addition(userInput1, userInput2);
+
+    cout << "The answer is " << result << endl;
+
     return 0;
 }
+
